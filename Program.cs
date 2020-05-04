93fe4eaf0ae41f518b5c52d61892b6d4e628bbdd
unknown <nkosimova06@gmail.com>
2020-05-05 02:09:48 +0500

    start rewriting all methods, change algorithm

--- Program.cs
+++ Program.cs
@@ -7,12 +7,12 @@ namespace MRain
 {
     class Program
     {
-        static int height = 14;
+        static int height = 15;
         static char[] SingleColumn = new char[height];
         static int InitRow;
         static int InitColumn;
         static object locker = new object(), locker1 = new object();
-        static bool currentstatus = true;
+      //  static bool currentstatus = true;
         static void Main(string[] args)
         {
             Random randomheight = new Random();
@@ -21,18 +21,21 @@ namespace MRain
             CleanColumn();
             InitColumn = Console.CursorLeft;
             InitRow = Console.CursorTop;
-            Task[] tasks = new Task[10];
+            Task[] tasks = new Task[2];
 
             for (int j = 0; j < tasks.Length; j++)
             {
                 object k = 2*j;
-                 int letterstreamheight = randomheight.Next(8,10); //длина потока букв
+                int letterstreamheight = randomheight.Next(4, 7); //длина потока букв
                 tasks[j] = Task.Factory.StartNew(() =>{ 
                     while (true)
                     { 
-                        while (currentstatus)  
-                            WriteStream(k,8,0,(int)height / 2-2);
-                        WriteStreamAsync(k,letterstreamheight,(int)height / 2-2, height);
+                        TopStream(k,5,0,(int)height/2-1);
+                       // while (currentstatus)  
+                       // TopStreamAsync(k,letterstreamheight,0,(int)height / 2);
+                       // currentstatus = true;
+                       // TopStreamAsyncAsync(k,letterstreamheight,(int)height / 2, height);
+
                     }
                 });
             }
@@ -45,7 +48,7 @@ namespace MRain
         }
         public static void CleanColumn(int b, int e)
         {
-            for (int i = b; i < e; i++)
+            for (int i = b; i <= e; i++)
                 SingleColumn[i] = ' ';
 
         }
@@ -68,67 +71,104 @@ namespace MRain
                 Point point = new Point(column,i);
                 WriteAt(SingleColumn[i], point);
             }
-            Thread.Sleep(200);   
+            Thread.Sleep(1000);   
         }
         //вывод цепочки({переписывать лист + показывать лист} - н раз)
-        public static void WriteStream(object column, int letterstreamheight, int start, int finish)
+        public static void TopStream(object column, int letterstreamheight, int beginpos,int endpos)
         {
             Random randomletter = new Random();
             Random randomheight = new Random();
             Point point = new Point(0,0);
-            int i, beginpos, endpos;
+            int i, firstletterpos = 0, lastletterpos = 0;
             point.col = (int)column;
-      
-            for (i = start; i <= finish + letterstreamheight; i++)
+            
+            for (i = beginpos; i <= endpos; i++)
             {
                 int last = 0, prelast = 0;
                 lock(locker) {
-                beginpos = (i <= letterstreamheight) ? start : i - letterstreamheight;
-                endpos = (i <= finish)? i : finish;
-                    for (int letterpos = beginpos; letterpos < endpos; letterpos++)
-                    {       
+                    firstletterpos = (i <= letterstreamheight) ? beginpos : i - letterstreamheight;
+                    lastletterpos = (i <= endpos)? i : endpos;
+                    for (int letterpos = firstletterpos; letterpos < lastletterpos; letterpos++)
+                    {         
                         char letter = Convert.ToChar(randomletter.Next(65,90));
                         SingleColumn[point.row + letterpos] = letter; 
                     }
-                prelast = point.row + endpos - 2;
-                last = point.row + endpos - 1;
+                prelast = point.row + lastletterpos - 2;
+                last = point.row + lastletterpos - 1;
                 WriteList((int) column, prelast, last);
-                CleanColumn(start, finish);
-                if (i == finish)
-                    currentstatus = false;
+                CleanColumn(beginpos, endpos);
                 }
             }
         }
-        //ассинхронный метод для вывода оставшейся цепочки
-         public static async void WriteStreamAsync(object column, int letterstreamheight, int start, int finish)
+      /*  public static async void TopStreamAsync(object column, int letterstreamheight, int start, int finish)
         {
             await Task.Run(() => 
             {
             Random randomletter = new Random();
             Random randomheight = new Random();
             Point point = new Point(0,0);
-            int i, beginpos, endpos;
+            int i, firstletterpos, lastletterpos;
             point.col = (int)column;
       
+
+            for (i = start; i <= finish+letterstreamheight; i++)
+            {
+               int last = 0, prelast = 0;
+                lock(locker) {
+                firstletterpos = (i <= letterstreamheight) ? start : i - letterstreamheight;
+                lastletterpos = (i <= finish)? i : finish;
+                    for (int letterpos = firstletterpos; letterpos < lastletterpos; letterpos++)
+    
+                    {       
+    
+                        char letter = Convert.ToChar(randomletter.Next(65,90));
+                        SingleColumn[point.row + letterpos] = letter; 
+                    }
+                prelast = point.row + lastletterpos - 2;
+                last = point.row + lastletterpos - 1;
+                WriteList(
+                    (int) column, prelast, last);
+                if (i =
+                = finish) DownStreamAsync(column, letterstreamheight, finish, height);
+                CleanColumn(start, finish);
+                }
+            } });
+        }
+        //ассинхронный метод для вывода оставшейся цепочки
+         public static async void DownStreamAsync(object column, int letterstreamheight, int start, int finish)
+        {
+            await Task.Run(() => 
+            {
+            Random randomletter = new Random();
+            Random randomheight = new Random();
+            Point point = new Point(0,0);
+            int i, firstletterpos, lastletterpos;
+            point.col = (int)column;
+      
+
             for (i = start; i <= finish + letterstreamheight; i++)
             {
                 int last = 0, prelast = 0;
                 lock(locker) {
-                beginpos = (i <= letterstreamheight) ? start : i - letterstreamheight;
-                endpos = (i <= finish)? i : finish;
-                    for (int letterpos = beginpos; letterpos < endpos; letterpos++)
+                firstletterpos = (i <= letterstreamheight) ? start : i - letterstreamheight;
+                lastletterpos = (i <= finish)? i : finish;
+                    for (int letterpos = firstletterpos; letterpos < lastletterpos; letterpos++)
+    
                     {       
+    
                         char letter = Convert.ToChar(randomletter.Next(65,90));
                         SingleColumn[point.row + letterpos] = letter; 
                     }
-                prelast = point.row + endpos - 2;
-                last = point.row + endpos - 1;
-                WriteList((int) column, prelast, last);
-                CleanColumn(start, finish);
+                prelast = point.row + lastletterpos - 2;
+                last = point.row + lastletterpos - 1;
+                WriteList(
+                    (int) column, prelast, last);
+                CleanCo
+                lumn(start, finish);
                 }
             }
             });
-        }
+        }*/    
     }
 }
 
