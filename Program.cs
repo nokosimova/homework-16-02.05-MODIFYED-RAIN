28d9e6bf8e6a1edae1b4e662fe9ebfbe95d43b6e
unknown <nkosimova06@gmail.com>
2020-05-04 20:45:50 +0500

    create project, add previous-homework's code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MRain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
